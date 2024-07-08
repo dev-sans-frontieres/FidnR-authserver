1380c01f8ae7ed6b92c8835bd30838bd898205dd
Chine Dmitri <76706642+ChineDmitri@users.noreply.github.com>
2024-07-08 20:57:43 +0200

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,5 +1,4 @@
 mvn spring-boot:run
--Dkeycloak.connectionsJpa.url=jdbc:mysql://mysql01.dev.apave.grp/kc_dev
 -Dkeycloak.connectionsJpa.driver=com.mysql.cj.jdbc.Driver
 -Dkeycloak.connectionsJpa.driverDialect=org.hibernate.dialect.MySQL8Dialect
 -Dkeycloak.connectionsJpa.user=dev
